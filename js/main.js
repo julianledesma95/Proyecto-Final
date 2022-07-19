6660243565e74de5b0ce1b7e199e01531ebeb3b2
Julian Ignacio Ledesma <julian.ledesma95@gmail.com>
2022-07-18 22:29:57 -0300

    Correcciones 1er Entrega

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* DESAFÍO COMPLEMENTARIO : Desafío: Incorporar Arrays */
 
-let productos = [{id:"1",producto:"Vino Tinto Malbec Cafayate"}, {id:"2",producto:"Vino Tinto Malbec Luigi Bosca"}, {id:"3",producto:"Vino Tinto Malbec Trumpeter"}, 
+/* let productos = [{id:"1",producto:"Vino Tinto Malbec Cafayate"}, {id:"2",producto:"Vino Tinto Malbec Luigi Bosca"}, {id:"3",producto:"Vino Tinto Malbec Trumpeter"}, 
 {id:"4",producto:"Vino Tinto Malbec Rutini"}, {id:"5",producto:"Vino Tinto Malbec Saint Felicien"}, {id:"6",producto:"Vino Tinto Cabernet Suavignon Cafayate"}, 
 {id:"7",producto:"Vino Tinto Cabernet Suavignon Luigi Bosca"}, {id:"8",producto:"Vino Tinto Cabernet Suavignon Trumpeter"}, {id:"9",producto:"Vino Tinto Cabernet Suavignon Rutini"}, 
 {id:"10",producto:"Vino Tinto Cabernet Suavignon Saint Felicien"}, {id:"11",producto:"Gin Bombay Sapphire"}, {id:"12",producto:"Gin Beefeater"}, {id:"13",producto:"Gin Gordons"}, 
@@ -32,4 +32,25 @@ const agregarProd = () => {
     }
 
     listaProductos.forEach(element => console.log(element));
+} */
+
+/* FIN DESAFÍO COMPLEMENTARIO : Desafío: Incorporar Arrays */
+let contenedorProductos = document.getElementById('contenedor-productos')
+
+mostrarProductos()
+
+function mostrarProductos(){
+    stockProductos.forEach(item =>{
+        let div = document.createElement('div')
+        div.innerHTML = `<div class="card">
+                            <img src="${item.img}" class="card-img-top" alt="producto1">
+                            <div class="card-body">
+                            <h5 class="card-title">${item.producto}</h5>
+                            <p class="card-text">${item.desc}</p>
+                            <p class="card-text">$${item.precio}</p>
+                            <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+                        </div>
+                        `
+            contenedorProductos.appendChild(div)
+    })
 }
